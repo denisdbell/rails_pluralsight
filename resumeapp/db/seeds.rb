a1996fb29ec3832ddc98025a2059b7a960eac936
denisdbell <denisdbell>
2017-06-14 18:45:07 -0500

    added seed data for resume

--- resumeapp/db/seeds.rb
+++ resumeapp/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#prevent duplication of data
+if Resume.count == 0
+    Resume.create([{name: 'Denis Bell', phone: '555-555-5555'},{name: 'Emanuel Brown', phone: '555-555-5555'}])
+end
