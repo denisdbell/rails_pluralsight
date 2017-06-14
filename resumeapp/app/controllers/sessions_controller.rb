3a1a41c287b3eb938e42ef40795f72ede70c5707
denisdbell <denisdbell>
2017-06-13 23:00:21 -0500

    first attempt at added the sessions create function

--- resumeapp/app/controllers/sessions_controller.rb
+++ resumeapp/app/controllers/sessions_controller.rb
@@ -1,8 +1,17 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
-  end
+
+      @user = User.where(username: params[:session][:username],params[:session][:password]).exists?(conditions = :none)
+     
+     # puts params[:session][:username]
+
+      if @user
+          puts "HELLO"
+      end  
+   end
 
 end
