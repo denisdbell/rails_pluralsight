d82404300960ec92d1e10b664e47960850f4406b
denisdbell <denisdbell>
2017-06-15 21:26:59 -0500

    updated schema as a result of migration

--- resumeapp/db/schema.rb
+++ resumeapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614225953) do
+ActiveRecord::Schema.define(version: 20170616013322) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "company_name"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20170614225953) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "view_histories", force: :cascade do |t|
+    t.integer  "resume_id"
+    t.datetime "time_viewed"
+    t.string   "ip"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
