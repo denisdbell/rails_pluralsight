d9a65ab34ba26894ce4e4492c0b7799d0c8462c2
denisdbell <denisdbell>
2017-06-17 17:44:57 -0500

    refactored core library

--- easy-url-monitor/easy-mon-lib.rb
+++ easy-url-monitor/easy-mon-lib.rb
@@ -9,72 +9,34 @@
 require 'fileutils'
 require 'net/http'
 require 'json'
-
-
-#Creates the default configuration file
-def create_config(config_filename)
-    
-     puts "Creating " + config_filename + " file"
-    
-     FileUtils.touch(config_filename)
-
-     #Load default configuration for config.json
-     File.open(config_filename, "w") do |f|
-        f.write '{
-                    "config": { 
-                                "email":{
-                                    "smtpServer":"smtp.gmail.com", 
-                                    "smtpPort":465,
-                                    "sslRequired":true,
-                                    "username":"username@gmail.com",
-                                    "password":"password@gmail.com"
-                                },
-                                "urls":[
-                                    {
-                                      "value":"http://www.google.com"
-                                    },
-                                    {
-                                      "value":"http://www.facebook.com"
-                                    },
-                                    {
-                                      "value":"http://www.twitter.com"
-                                    }
-
-                                ]
-
-                            }
-                 }'
-     end
-
-end
-
+require 'securerandom'
+require "./configuration"
+require "./emailer"
 
 #Starts the monitoring process
-def start_monitoring(config_filename)
+def start_monitoring
 
     #Read configuration
-    config_file = File.open(config_filename)
-
-    config = JSON.parse(config_file.read)
-
-    #Url parsing
-
+    config = read_configuration_file
 
     while true
         
         for url in config['config']['urls']
-            
-            puts url['value']
 
-            url = URI.parse(url['value'])
+            new_url = URI.parse(url['value'] + "?#{SecureRandom.uuid}" ) #Add UUID to prevent caching
 
-            request = Net::HTTP::Get.new(url.to_s)
+            puts "#{url['value']}?#{SecureRandom.uuid}"
 
-            response = Net::HTTP.start(url.host, url.port) {|http|
+            request = Net::HTTP::Get.new(new_url.to_s)
+
+            response = Net::HTTP.start(new_url.host, new_url.port) {|http|
                 http.request(request)
             }
             
-            puts config['config']['urls'][0]['value']  + "responded with a status of" + response.to_s 
+           send_email
+           
+           puts "responded with a status of #{  response.code } "
+        
         end
 
     end
@@ -88,22 +50,21 @@ def command_line_input_validation(command)
 
         puts         "
 
-                          INVALID COMMAND LINE ARGUMENT!!!!!!  
+                          INVALID COMMAND LINE ARGUMENT!!!!!! 
 
-                          <" + command  +"> is not a recognized command. The list of accepted command are below:
+                          <-- #{ command } --> is not a recognized command. The list of accepted command are below:
                           
                           init - Creates the config.json file with default vaulues:
 
                           start- Starts the monitoring of the URLS 
 
-
-
-                        "
+                    "
         return
-        
+
     end 
 
 end
+
 
 
 
