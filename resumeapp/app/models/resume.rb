382c0db04dc1c00052d5c328e1b04cf60ff0b6f5
denisdbell <denisdbell>
2017-06-16 20:42:29 -0500

    added necessary associations to the model

--- resumeapp/app/models/resume.rb
+++ resumeapp/app/models/resume.rb
@@ -1,5 +1,21 @@
 class Resume < ApplicationRecord
 
+
+    before_save :do_logging
+
+    def db_logging
+        puts "Saving Object"
+    end
+
     has_many :view_histories
+
+    has_many :jobs
+
+    has_one :cover_letter
+
+    has_and_belongs_to_many :employers
     
+    has_many :applications
+
+    has_many :employers :through => :applications
 end
