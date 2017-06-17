955e0c8cf14adf4a92e555bc178daa96ba36d602
denisdbell <denisdbell>
2017-06-17 17:45:07 -0500

    added email library

---
+++ easy-url-monitor/emailer.rb
@@ -0,0 +1,38 @@
+require 'mail'
+require "./configuration"
+
+message = <<MESSAGE_END
+From: Private Person <me@fromdomain.com>
+To: A Test User <test@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+MESSAGE_END
+
+
+def send_email
+
+        config = read_configuration_file
+
+        smtpServer = config['config']['smtp']['smtpServer'] 
+        smtpPort = config['config']['smtp']['smtpPort'] 
+        username = config['config']['smtp']['username'] 
+        password = config['config']['smtp']['password'] 
+
+
+        options = { :address              => smtpServer,
+                    :port                 => smtpPort,
+                    :domain               => 'your.host.name',
+                    :user_name            =>  username,
+                    :password             =>  password,
+                    :authentication       => 'plain',
+                    :enable_starttls_auto => true  }
+
+
+
+        Mail.defaults do
+             delivery_method :smtp, options
+        end
+end
+
+
